1f8d2452ed66c166a63b6ff2fb0253938d8d6430
Gabriela Josebachvili <gabbyjose@gmail.com>
2017-08-27 17:44:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  Object.assign({}, recipes, {key: value});
+  object = Object.assign({}, object, {[key]: value});
+  return object;
 }
 
 function deleteFromObjectByKey(object, key){
