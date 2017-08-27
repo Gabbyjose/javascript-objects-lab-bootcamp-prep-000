c69a7cc5d7638cea986b2122a262ea49df4aace8
Gabriela Josebachvili <gabbyjose@gmail.com>
2017-08-27 17:43:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var newObj = Object.assign({}, recipes, {key: value});
+  var newObj = Object.assign({}, recipes, {[key]: value});
   return newObj;
 }
 
